54ac49bdd6308bb3f3cb96e11a139f10e054e58d
emmanuellar <emmanuellar805@gmail.com>
2020-01-09 03:28:37 +0100

    added image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import img2 from './img2.jpg';
 import './App.css';
 
 function App() {
@@ -8,12 +8,12 @@ function App() {
         <div className="container">
           <div className="card">
             <div className="card-content">
-                <img src="img2.JPG" alt="Avatar"/>
+                <img src={img2} alt="Avatar"/>
                 <div className="text">
                   <h4><b>AMARACHI AZUBUIKE</b></h4> 
                   <p>Front End Web Developer <br/> (HTML, CSS, BS4, JS, React)</p>
-                <button>Send Message</button>
                 </div>
+                 <button>Send Message</button>
               </div>
           </div>
       </div>
